a52d7870578e8e32ac6325cd2ad123ed78d1deb5
tmhuyen <21127202@student.hcmus.edu.vn>
2024-06-29 16:52:32 +0700

    update frontend

--- app.js
+++ app.js
@@ -1,62 +1,15 @@
-const createError = require('http-errors');
 const express = require('express');
-const session = require('express-session');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const passport = require('passport');
-const env = require("dotenv");
-const mongoose = require("mongoose");
-const cors = require('cors');
-const fs = require("fs");
+
+// Import required modules
 const app = express();
-const {initializeHandlebars, compile} = require("./config/handlebars");
-env.config();
 
-mongoose
-    .connect(process.env.DB_URI)
-    .then(() => console.log("DB Connection Successfully!"))
-    .catch((err) => {
-        console.log(err);
-    });
-
-app.use(cors());
-// View engine
-app.set('view engine', 'handlebars');
-app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json({ limit: '20mb' }));
-app.use(express.urlencoded({ limit: '20mb', extended: true }));
-app.use(cookieParser());
-
-// import configurations
-require('./config/passport');
-initializeHandlebars();
-
-app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.SESSION_SECRET,
-    cookie: { maxAge: 1000 * 60 * 15 }
-}))
-
-app.use(passport.initialize())
-app.use(passport.session())
-
-const indexRouter = require("./routes/index_router");
-const adminRouter = require("./routes/admin");
-
-app.use('/', indexRouter);
-
-app.get('/testing', function (req, res, next) {
-    res.status(200).send(compile('pages/index.hbs', {
-        layout: 'index.hbs'
-    }))
-})
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
+// Define routes
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
 });
 
-module.exports = app;
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
